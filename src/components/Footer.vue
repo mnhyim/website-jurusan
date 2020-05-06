dc5999f225ff574005692841b8a503dc18239cd9
Muhamad Nur Hasyim <mnhyim76@gmail.com>
2020-05-06 20:36:37 +0800

    fix footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,81 +1,114 @@
 <template>
+  <div class="">
     <footer class="footer has-background-primary has-text-white">
-    <div class="columns">
-      <div class="column is-4">
-        <h2 class="title is-4 has-text-white has-bottom-border">Sosial Media</h2>
-        <ul>
-          <li><a href="https://facebook.com" class="has-text-white">Facebook</a></li>
-          <li><a href="https://twitter.com" class="has-text-white">Twitter</a></li>
-          <li><a href="https://instagram.com" class="has-text-white">Instagram</a></li>
-        </ul>
-      </div>
-      <div class="column is-4">
-        <h2 class="title is-4 has-text-white has-bottom-border">Layanan</h2>
-        <ul>
-          <li><a href="http://sikantan.itk.ac.id/" class="has-text-white">SIKANTAN</a></li>
-          <li><a href="http://mail.itk.ac.id/" class="has-text-white">ITK Webmail  </a></li>
-          <li><a href="http://pmb.itk.ac.id/" class="has-text-white">Penerimaan Mahasiswa Baru</a></li>
-        </ul>
-      </div>
-      <div class="column is-4">
-        <h2 class="title is-4 has-text-white has-bottom-border">Temukan Kami</h2>
-        <div style="width: 100%"><iframe width="100%" height="350" src="https://maps.google.com/maps?width=100%&amp;height=350&amp;hl=en&amp;coord=-1.15041555, 116.8614686&amp;q=Jl.%20Soekarno-Hatta%20Km.%2015%2C%20Karang%20Joang%2C%20%20%20%20%20%20%20%20%20%20%20%20Balikpapan%2C%20Kalimantan%20Timur%2C%2076127+(Institut%20Teknologi%20Kalimantan)&amp;ie=UTF8&amp;t=&amp;z=15&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"><a href="https://www.maps.ie/draw-radius-circle-map/">Radius map tool</a></iframe></div>
-      </div>
-      <!-- <div class="column is-4 has-background-primary-darker">
-        <h2 class="title is-4 has-text-white has-bottom-border">Hubungi Kami</h2>
-        <div>
-          <div class="field is-horizontal">
-            <div class="field-body">
-              <div class="field">
-                <label class="label has-text-white">Nama</label>
-                <div class="control">
-                  <input class="input" type="text" placeholder="Nama Lengkap">
-                </div>
-              </div>
-              <div class="field">
-                <label class="label has-text-white">NIM</label>
-                <div class="control">
-                  <input class="input" type="text" placeholder="Nomor Induk Mahasiswa">
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="field">
-            <label class="label has-text-white">Email</label>
-            <div class="control">
-              <input class="input" type="email" placeholder="Email">
-            </div>
-          </div>
-          <div class="field">
-            <label class="label has-text-white">Pesan</label>
-            <div class="control">
-              <textarea class="textarea" placeholder="Pesan"></textarea>
-            </div>
-          </div>
-          <div class="field">
-            <div class="control">
-              <label class="checkbox">
-                <input type="checkbox">
-                I agree to the <a href="#">terms and conditions</a>
-              </label>
-            </div>
-          </div>
-          <div class="field is-grouped">
-            <div class="control">
-              <button class="button is-link">Submit</button>
-            </div>
-            <div class="control">
-              <button class="button is-link is-light">Cancel</button>
-            </div>
-          </div>
+      <div class="columns">
+        <div class="column is-4">
+          <h2 class="title is-4 has-text-white has-bottom-border">Sosial Media</h2>
+          <ul>
+            <li>
+              <a href="https://www.facebook.com/ITKalimantan/" class="has-text-white">
+                <b-icon pack="fab" icon="facebook" size="is-small"/>Facebook
+              </a>
+            </li>
+            <li>
+              <a href="https://twitter.com/itk_official_" class="has-text-white">
+                <b-icon pack="fab" icon="twitter" size="is-small"/>Twitter
+              </a>
+            </li>
+            <li>
+              <a href="https://www.instagram.com/itk_official/" class="has-text-white">
+                <b-icon pack="fab" icon="instagram" size="is-small"/>Instagram
+              </a>
+            </li>
+          </ul>
         </div>
-      </div> -->
-    </div>
-  </footer>
+        <div class="column is-4">
+          <h2 class="title is-4 has-text-white has-bottom-border">Layanan</h2>
+          <ul>
+            <li>
+              <a href="http://sikantan.itk.ac.id/" class="has-text-white">
+                <b-icon pack= "fas" icon="link" size="is-small"/>SIKANTAN
+              </a>
+            </li>
+            <li>
+              <a href="http://mail.itk.ac.id/" class="has-text-white">
+                <b-icon pack= "fas" icon="link" size="is-small"/>ITK Webmail
+              </a>
+            </li>
+            <li>
+              <a href="http://pmb.itk.ac.id/" class="has-text-white">
+                <b-icon pack= "fas" icon="link" size="is-small"/>Penerimaan Mahasiswa Baru
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div class="column is-4">
+          <h2 class="title is-4 has-text-white has-bottom-border">Temukan Kami</h2>
+          <div style="width: 100%"><iframe width="100%" height="350" src="https://maps.google.com/maps?width=100%&amp;height=350&amp;hl=en&amp;coord=-1.15041555, 116.8614686&amp;q=Jl.%20Soekarno-Hatta%20Km.%2015%2C%20Karang%20Joang%2C%20%20%20%20%20%20%20%20%20%20%20%20Balikpapan%2C%20Kalimantan%20Timur%2C%2076127+(Institut%20Teknologi%20Kalimantan)&amp;ie=UTF8&amp;t=&amp;z=15&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"><a href="https://www.maps.ie/draw-radius-circle-map/">Radius map tool</a></iframe></div>
+        </div>
+        <!-- <div class="column is-4 has-background-primary-darker">
+          <h2 class="title is-4 has-text-white has-bottom-border">Hubungi Kami</h2>
+          <div>
+            <div class="field is-horizontal">
+              <div class="field-body">
+                <div class="field">
+                  <label class="label has-text-white">Nama</label>
+                  <div class="control">
+                    <input class="input" type="text" placeholder="Nama Lengkap">
+                  </div>
+                </div>
+                <div class="field">
+                  <label class="label has-text-white">NIM</label>
+                  <div class="control">
+                    <input class="input" type="text" placeholder="Nomor Induk Mahasiswa">
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="field">
+              <label class="label has-text-white">Email</label>
+              <div class="control">
+                <input class="input" type="email" placeholder="Email">
+              </div>
+            </div>
+            <div class="field">
+              <label class="label has-text-white">Pesan</label>
+              <div class="control">
+                <textarea class="textarea" placeholder="Pesan"></textarea>
+              </div>
+            </div>
+            <div class="field">
+              <div class="control">
+                <label class="checkbox">
+                  <input type="checkbox">
+                  I agree to the <a href="#">terms and conditions</a>
+                </label>
+              </div>
+            </div>
+            <div class="field is-grouped">
+              <div class="control">
+                <button class="button is-link">Submit</button>
+              </div>
+              <div class="control">
+                <button class="button is-link is-light">Cancel</button>
+              </div>
+            </div>
+          </div>
+        </div> -->
+      </div>
+    </footer>
+    <p class="footer-text has-text-white has-background-dark is-size-7">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia alias illo possimus ipsum expedita rem numquam dolorem, veritatis earum velit aspernatur consectetur voluptates nostrum perferendis rerum repellat provident facilis labore!</p>
+  </div>
 </template>
 
 <style scoped>
   .column{
     text-align: left;
+  }
+  .icon{
+    margin: 0 12px 0 0;
+  }
+  .footer-text{
+    padding: 4px;
   }
 </style>
