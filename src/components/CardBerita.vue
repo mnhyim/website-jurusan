a7343b84914a589828e6d716dde218cee51cae95
Muhamad Nur Hasyim <mnhyim76@gmail.com>
2020-05-20 18:46:35 +0800

    fixing card berita variable

--- src/components/CardBerita.vue
+++ src/components/CardBerita.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="card">
     <header class="card-header has-background-jmti">
-      <p class="card-header-title has-text-white">{{berita.title}}</p>
+      <p class="card-header-title has-text-white">{{beritabs.title}}</p>
     </header>
     <div class="card-content">
       <figure class="image is-3by2 gmbr">
-        <img v-bind:src="require(`@/assets/berita/${berita.images}`)" />
+        <img v-bind:src="require(`@/assets/berita/${beritabs.images}`)" />
       </figure>
-      <p>{{berita.berita}}</p>
+      <p>{{beritabs.berita}}</p>
     </div>
     <footer class="card-footer">
-      <a v-bind:href="berita.link" class="card-footer-item">Baca lebih lengkap</a>
+      <a v-bind:href="beritabs.link" class="card-footer-item">Baca lebih lengkap</a>
     </footer>
   </div>
 </template>
 <script>
 export default {
   props:[
-    "berita"
+    "beritabs"
   ]
 }
 </script>
