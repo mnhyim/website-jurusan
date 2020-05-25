e7fa4b52e44a44a82a63ece9b23dafd8608f95d3
Muhamad Nur Hasyim <mnhyim76@gmail.com>
2020-05-25 13:23:33 +0800

    resizing footer text size

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,8 +3,8 @@
     <footer class="footer has-background-primary has-text-white">
       <div class="columns">
         <div class="column is-4">
-          <h2 class="title is-4 has-text-white has-bottom-border">Sosial Media</h2>
-          <ul>
+          <h2 class="title is-5 has-text-white has-bottom-border">Sosial Media</h2>
+          <ul class="is-size-7">
             <li>
               <a href="https://www.facebook.com/ITKalimantan/" class="has-text-white" target="_blank">
                 <b-icon pack="fab" icon="facebook" size="is-small"/>Facebook
@@ -23,8 +23,8 @@
           </ul>
         </div>
         <div class="column is-4">
-          <h2 class="title is-4 has-text-white has-bottom-border">Layanan</h2>
-          <ul>
+          <h2 class="title is-5 has-text-white has-bottom-border">Layanan</h2>
+          <ul class="is-size-7">
             <li>
               <a href="http://sikantan.itk.ac.id/" class="has-text-white" target="_blank">
                 <b-icon pack= "fas" icon="link" size="is-small"/>SIKANTAN
@@ -41,9 +41,24 @@
               </a>
             </li>
           </ul>
+          <h2 class="title is-5 has-text-white kontak-title">Kontak</h2>
+          <ul class="is-size-7">
+            <li>
+              <b-icon pack= "fas" icon="map" size="is-small"/>Jl. Soekarno-Hatta Km. 15, Karang Joang, Balikpapan, Kalimantan Timur, 76127
+            </li>
+            <li>
+              <b-icon pack= "fas" icon="phone" size="is-small"/>0542-8530801
+            </li>
+            <li>
+              <b-icon pack= "fas" icon="phone" size="is-small"/>0542-8530800
+            </li>
+            <li>
+              <b-icon pack= "fas" icon="envelope" size="is-small"/>humas@itk.ac.id
+            </li>
+          </ul>
         </div>
         <div class="column is-4">
-          <h2 class="title is-4 has-text-white has-bottom-border">Temukan Kami</h2>
+          <h2 class="title is-5 has-text-white has-bottom-border">Temukan Kami</h2>
           <div style="width: 100%"><iframe width="100%" height="350" src="https://maps.google.com/maps?width=100%&amp;height=350&amp;hl=en&amp;coord=-1.15041555, 116.8614686&amp;q=Jl.%20Soekarno-Hatta%20Km.%2015%2C%20Karang%20Joang%2C%20%20%20%20%20%20%20%20%20%20%20%20Balikpapan%2C%20Kalimantan%20Timur%2C%2076127+(Institut%20Teknologi%20Kalimantan)&amp;ie=UTF8&amp;t=&amp;z=15&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"><a href="https://www.maps.ie/draw-radius-circle-map/">Radius map tool</a></iframe></div>
         </div>
         <!-- <div class="column is-4 has-background-primary-darker">
@@ -110,5 +125,9 @@
   }
   .footer-text{
     padding: 8px;
+  }
+  .kontak-title{
+    margin-top: 16px;
+    border-bottom: 3px solid white;
   }
 </style>
