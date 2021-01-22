9ae70f0d4f1ea19d46c12b5c7148ceb88dce828b
erzaffasya <58721335+erzaffasya@users.noreply.github.com>
2021-01-22 22:31:41 +0700

    sad

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -61,55 +61,6 @@
           <h2 class="title is-5 has-text-white has-bottom-border">Temukan Kami</h2>
           <div style="width: 100%"><iframe width="100%" height="350" src="https://maps.google.com/maps?width=100%&amp;height=350&amp;hl=en&amp;coord=-1.15041555, 116.8614686&amp;q=Jl.%20Soekarno-Hatta%20Km.%2015%2C%20Karang%20Joang%2C%20%20%20%20%20%20%20%20%20%20%20%20Balikpapan%2C%20Kalimantan%20Timur%2C%2076127+(Institut%20Teknologi%20Kalimantan)&amp;ie=UTF8&amp;t=&amp;z=15&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"><a href="https://www.maps.ie/draw-radius-circle-map/">Radius map tool</a></iframe></div>
         </div>
-        <!-- <div class="column is-4 has-background-primary-darker">
-          <h2 class="title is-4 has-text-white has-bottom-border">Hubungi Kami</h2>
-          <div>
-            <div class="field is-horizontal">
-              <div class="field-body">
-                <div class="field">
-                  <label class="label has-text-white">Nama</label>
-                  <div class="control">
-                    <input class="input" type="text" placeholder="Nama Lengkap">
-                  </div>
-                </div>
-                <div class="field">
-                  <label class="label has-text-white">NIM</label>
-                  <div class="control">
-                    <input class="input" type="text" placeholder="Nomor Induk Mahasiswa">
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="field">
-              <label class="label has-text-white">Email</label>
-              <div class="control">
-                <input class="input" type="email" placeholder="Email">
-              </div>
-            </div>
-            <div class="field">
-              <label class="label has-text-white">Pesan</label>
-              <div class="control">
-                <textarea class="textarea" placeholder="Pesan"></textarea>
-              </div>
-            </div>
-            <div class="field">
-              <div class="control">
-                <label class="checkbox">
-                  <input type="checkbox">
-                  I agree to the <a href="#">terms and conditions</a>
-                </label>
-              </div>
-            </div>
-            <div class="field is-grouped">
-              <div class="control">
-                <button class="button is-link">Submit</button>
-              </div>
-              <div class="control">
-                <button class="button is-link is-light">Cancel</button>
-              </div>
-            </div>
-          </div>
-        </div> -->
       </div>
     </footer>
     <p class="footer-text has-text-white has-background-dark is-size-7">Copyright &copy; 2020 Kelompok 1. All rights reserved.</p>
